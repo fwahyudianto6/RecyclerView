39c607adf687806facc7f47b99aeefdbbefe75aa
fwahyudianto06 <fwahyudi06@gmail.com>
2019-04-30 22:58:38 +0700

    Added Menu Options

--- app/src/main/java/com/fwahyudianto/learn/president/MainActivity.java
+++ app/src/main/java/com/fwahyudianto/learn/president/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.fwahyudianto.learn.president.controllers.PresidentListAdapter;
 import com.fwahyudianto.learn.president.models.President;
@@ -32,5 +34,26 @@ public class MainActivity extends AppCompatActivity {
         PresidentListAdapter oPresidentListAdapter = new PresidentListAdapter(this);
         oPresidentListAdapter.setPresident(alData);
         oRecyclerView.setAdapter(oPresidentListAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu p_oMenu) {
+        getMenuInflater().inflate(R.menu.menu_main, p_oMenu);
+
+        return  super.onCreateOptionsMenu(p_oMenu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem p_oMenuItem) {
+        switch (p_oMenuItem.getItemId()) {
+            case R.id.action_list:
+                break;
+            case R.id.action_grid:
+                break;
+            case  R.id.action_cardview:
+                break;
+        }
+
+        return super.onOptionsItemSelected(p_oMenuItem);
     }
 }
